fbf9eba3148b5598b0ed9aece9d7c97237f8ae21
mar1fatih <marouanefatih631@gmail.com>
2023-09-05 18:06:34 +0000

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def islower(d):
+    if ord(d) >= 97 and ord(d) <= 123:
+        return True
+    else:
+        return False
+
+def uppercase(str):
+    for d in str:
+        print("{:d}".format(ord(d) - 32) if islower(d) else ord(d), end="")
+    print("")

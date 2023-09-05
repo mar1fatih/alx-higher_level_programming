ebc803dda5ce9a438d85a0ac2c73daca01463306
mar1fatih <marouanefatih631@gmail.com>
2023-09-05 18:27:15 +0000

    task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for b in range(1, 100 + 1):
+        if b % 5 == 0:
+            print("Buzz", end=" ")
+        elif b % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif b % 3 == 0:
+            print("Fizz", end=" ")
+        else:
+            print("{:d}".format(b), end=" ")

08cef6ad15dfa93070e48a30529950e7a36c28d0
mar1fatih <marouanefatih631@gmail.com>
2023-09-05 17:04:59 +0000

    task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+    modi = number % 10
+if number < 0:
+    modi = number % -10
+if number == 0:
+    modi = number
+print(f"last digit of {number:d} is {modi:d} and is", end=" ")
+if modi > 5:
+    print("greater than 5")
+elif modi == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")

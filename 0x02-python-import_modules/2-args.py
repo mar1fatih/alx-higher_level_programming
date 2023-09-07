c63444ef270e91a17bd37c278769197495d9b7f5
mar1fatih <marouanefatih631@gmail.com>
2023-09-07 20:57:26 +0000

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    c == 0
+
+    for i in argv:
+        c += 1
+    c -= 1
+    if c == 0:
+        print("0 arguments.")
+    elif c == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(c))
+    for i in range(c):
+        print("{}: {}".format(i + 1, argv[i + 1]))

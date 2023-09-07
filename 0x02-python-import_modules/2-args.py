1984f27375a972fdff7b523e798c61decb641668
mar1fatih <marouanefatih631@gmail.com>
2023-09-07 21:34:11 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
-if __name__ != "__main__":
-    exit()
-import sys
+if __name__ == "__main__":
+    import sys
 
-c = len(sys.argv)
-c -= 1
-if c == 0:
-    print("0 arguments.")
-elif c == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(c))
-for j in range(c):
-    print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    c = len(sys.argv)
+    c -= 1
+    if c == 0:
+        print("0 arguments.")
+    elif c == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(c))
+    for j in range(c):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))

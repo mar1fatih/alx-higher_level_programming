dc57a8d02d09b10c34489ea49ab8cc7b443ad621
mar1fatih <marouanefatih631@gmail.com>
2023-09-10 17:55:16 +0000

    task13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+ * is_palindrome - check the code
+ * @head: head
+ * Return: 0 if palindrome 1 if not
+*/
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = *head;
+	int *ptr = NULL;
+	int s = 0, i = 0;
+
+	if (!(*head))
+	{
+		return (1);
+	}
+	while(current)
+	{
+		current = current->next;
+		s++;
+	}
+	ptr = malloc(sizeof(int) * s);
+	if (!ptr)
+	{
+		perror("failed to allocate");
+		return (1);
+	}
+	current = *head;
+	while (current)
+	{
+		ptr[i] = current->n;
+		current = current->next;
+		i++;
+	}
+	current = *head;
+	s--;
+	while(current)
+	{
+		if (current->n != ptr[s])
+		{
+			return (0);
+		}
+		current = current->next;
+		s--;
+	}
+	free(ptr);
+	return (1);
+}

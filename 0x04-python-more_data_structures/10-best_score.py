3d5f6f9e0001eff3e16f874dac6cdf88e1b2b390
mar1fatih <marouanefatih631@gmail.com>
2023-09-15 14:58:49 +0000

    task10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,6 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     _maxv = 0
-    _maxk = 0
     for k, v in a_dictionary.items():
         if v > _maxv:
             _maxk = k

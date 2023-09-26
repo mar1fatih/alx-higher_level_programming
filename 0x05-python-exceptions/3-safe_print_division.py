359e8d09d13a86924507195504221f3dc1fa3dc4
mar1fatih <marouanefatih631@gmail.com>
2023-09-26 00:32:11 +0000

    task3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    i = 0
+    try:
+        i = a / b
+    except ZeroDivisionError:
+        i = None
+    finally:
+        print("Inside result: {}".format(i))
+        return i

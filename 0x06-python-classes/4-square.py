5e635fd702e3af6260c8d836ec5ef88fe9d2b8ab
mar1fatih <marouanefatih631@gmail.com>
2023-09-27 01:40:03 +0000

    task4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,10 +4,12 @@ class Square:
     def __init__(self, size=0):
         """init"""
         self.size = size
+
     @property
     def size(self):
         """size"""
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -15,6 +17,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """area"""
         return self.__size ** 2

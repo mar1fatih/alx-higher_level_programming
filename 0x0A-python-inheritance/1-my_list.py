28085968e140961a2ace04cfb4a4a522d0af9f23
mar1fatih <marouanefatih631@gmail.com>
2023-10-09 17:05:27 +0000

    task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""sorted list"""
+
+
+class MyList(list):
+    """MyList"""
+    def print_sorted(self):
+        """print_sorted"""
+        sorted_list = sorted(list)
+        print(sorted_list)

bdb691218c39cb876be1dec7b837e9093f19613c
mar1fatih <marouanefatih631@gmail.com>
2023-10-10 01:10:25 +0000

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -11,6 +11,6 @@ class Square(Rectangle):
         self.__size = size
         super().__init__(size, size)
 
-    def area(area):
+    def area(self):
         """area calc"""
         return self.__size ** 2

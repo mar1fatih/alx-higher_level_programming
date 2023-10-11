c323b18fd491ccaded270ac49cc80c8705b7eff3
mar1fatih <marouanefatih631@gmail.com>
2023-10-11 00:32:33 +0000

    task11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""10. Student to JSON with filter"""
+
+
+class Student:
+    """class Student"""
+    def __init__(self, first_name, last_name, age):
+        """__init__"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to_json"""
+        if attrs is None:
+            return self.__dict__
+        dict_d = self.__dict__
+        dict_t = dict()
+        for key, value in dict_d.items():
+            for attr in attrs:
+                if key == attr:
+                    dict_t.update({key: value})
+        return dict_t
+
+    def reload_from_json(self, json):
+        """reload_from_json"""
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value

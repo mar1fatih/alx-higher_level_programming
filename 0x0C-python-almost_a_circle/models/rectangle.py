131f5fc3d1fe6e826419110eb956712d9cd29db9
mar1fatih <marouanefatih631@gmail.com>
2023-10-16 16:22:00 +0000

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,17 +7,17 @@ class Rectangle(Base):
     """Rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
         """width getter"""
         return self.__width
 
-    @setter
+    @width.setter
     def width(self, value):
         """width setter"""
         self.vlidate("width", value)

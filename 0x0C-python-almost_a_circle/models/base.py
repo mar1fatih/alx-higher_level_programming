77bf515482beb58d0d978c44fc799dc1978c71a3
mar1fatih <marouanefatih631@gmail.com>
2023-11-13 17:40:08 +0000

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,11 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """convert list of dict to json str"""
+        if not list_dictionaries or len(list_dictionaries) == 0:
+            return "[]"
+        import json
+        return json.dumps(list_dictionaries)

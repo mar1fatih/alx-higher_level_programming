df908c4bc56113830573a58ef680a7322867655c
mar1fatih <marouanefatih631@gmail.com>
2023-11-13 17:50:55 +0000

    task 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,3 +36,13 @@ class Base:
         """extract the list from json str"""
         import json
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """create an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1, 0, 0, 0)
+        if cls.__name__ == "Square":
+            dummy = cls(1, 0, 0, 0)
+        dummy.update(**dictionary)
+        return dummy

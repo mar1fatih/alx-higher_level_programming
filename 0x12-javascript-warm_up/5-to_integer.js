a7b8933e732f349962086dcc68c9fb2234a48e45
mar1fatih <marouanefatih631@gmail.com>
2024-01-08 15:56:30 -0800

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let myNum;
 myNum = parseInt(process.argv[2]);
-if (!myNum) {
+if (!myNum || isNaN(myNum)) {
   console.log('Not a number');
 } else {
   console.log(`My number: ${myNum}`);

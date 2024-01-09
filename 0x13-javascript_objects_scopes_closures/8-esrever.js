cb7046f9fc5a7b797304682f2d7d9ad355cb5f5d
mar1fatih <marouanefatih631@gmail.com>
2024-01-09 14:19:26 -0800

    permission +x

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let b = list.length - 1;
+  let temp;
+  for (let i = 0; i < list.length / 2; i++) {
+    temp = list[i];
+    list[i] = list[b];
+    list[b] = temp;
+    b--;
+  }
+  return (list);
+}

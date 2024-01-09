85f0a34fed64d1533a2d66dbadc85a7144c69cdd
mar1fatih <marouanefatih631@gmail.com>
2024-01-09 13:49:24 -0800

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Sqr = require('./5-square');
+
+class Square extends Sqr {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;

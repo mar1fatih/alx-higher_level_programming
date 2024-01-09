03061d5aec470ef437021966c0dca9646e8306c6
mar1fatih <marouanefatih631@gmail.com>
2024-01-08 18:01:27 -0800

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const array = process.argv.slice(2).map(Number);
+  let secondLargest = array.sort((a, b) => b - a);
+  for (let i = 1; i < array.length; i++) {
+       if (array[i] !== array[0]) {
+         secondLargest = array[i];
+         break;
+     }
+   }
+  console.log(secondLargest);
+}

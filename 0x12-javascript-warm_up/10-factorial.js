f678e8558747d91c63a05f82f0f961696f519892
mar1fatih <marouanefatih631@gmail.com>
2024-01-09 18:45:50 -0800

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,12 +2,12 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log(1);
 } else {
-  fac = Factorial(Number(process.argv[2]));
+  const fac = Factorial(Number(process.argv[2]));
   console.log(fac);
 }
 function Factorial (num) {
   if (num === 0) {
     return (1);
   }
-  return (num * Factorial(num - 1))
+  return (num * Factorial(num - 1));
 }

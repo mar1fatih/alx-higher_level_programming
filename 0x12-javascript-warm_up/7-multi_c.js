9407404709f6be799a207e8d97f5ca29648de1c6
mar1fatih <marouanefatih631@gmail.com>
2024-01-09 18:35:53 -0800

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,6 +3,6 @@ if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
-  console.log('C is fun');
+    console.log('C is fun');
   }
 }

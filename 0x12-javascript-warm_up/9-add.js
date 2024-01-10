99f8a88f11b6080b3b8a8ec65a4336b5d7e95f02
mar1fatih <marouanefatih631@gmail.com>
2024-01-09 18:43:04 -0800

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,5 +2,5 @@
 function add (a, b) {
   return (a + b);
 }
-let result = add(Number(process.argv[2]), Number(process.argv[3]));
+const result = add(Number(process.argv[2]), Number(process.argv[3]));
 console.log(result);

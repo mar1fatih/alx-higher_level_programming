c940959e4daf09da549093c4cd59e3bc9143c223
mar1fatih <marouanefatih631@gmail.com>
2024-01-13 04:44:31 -0800

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
-                WHERE UPPER(SUBSTRING(* FROM 1 FOR 1)) = 'N';""")
+                WHERE name LIKE BINARY 'N%' ORDER BY states.id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)

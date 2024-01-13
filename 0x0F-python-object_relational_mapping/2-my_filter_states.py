5246c6f282fc4054f0fec251802eb4bc9ddf3ec0
mar1fatih <marouanefatih631@gmail.com>
2024-01-13 05:25:07 -0800

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
-                WHERE name = {}""".format(argv[4]))
+                WHERE name LIKE BINARY '{}'""".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)

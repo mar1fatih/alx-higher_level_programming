80321b4bce9c8934fb75bf442a309908ec42b9e1
mar1fatih <marouanefatih631@gmail.com>
2024-01-13 07:38:49 -0800

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
-    cur = db.cursor.()
+    cur = db.cursor()
     cur.execute("""SELECT cities.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id
                 WHERE states.name='{}'""".format(argv[4]))

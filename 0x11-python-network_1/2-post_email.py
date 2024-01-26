16c8b0e866fe353adfd2c815958c5aa5848c98f3
mar1fatih <marouanefatih631@gmail.com>
2024-01-26 11:19:02 -0800

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,11 +5,12 @@ import urllib.request
 from sys import argv
 
 
-url = argv[1]
-email = argv[2]
-value = {'email' : email}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    print(response.read().encode('utf-8'))
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    value = {'email' : email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().encode('utf-8'))

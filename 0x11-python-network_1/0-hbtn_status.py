4b61843770d12829a506983bb49d4366ddddefd4
mar1fatih <marouanefatih631@gmail.com>
2024-01-26 10:41:42 -0800

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# python script to request from url
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    request = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(request) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))

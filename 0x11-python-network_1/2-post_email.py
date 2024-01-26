52af0bd80c20d193b4a68dc1d91cd536a6e97bd4
mar1fatih <marouanefatih631@gmail.com>
2024-01-26 11:23:51 -0800

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """sends a POST request to the passed URL with the email as a parameter"""
-import urllib.parse
-import urllib.request
-from sys import argv
 
 
 if __name__ == "__main__":
+    import urllib.parse
+    import urllib.request
+    from sys import argv
+
     url = argv[1]
     email = argv[2]
     value = {'email' : email}
